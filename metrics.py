432b38f365074ab2d427b07b428fe4fdc72de813
Narayan Pai <narayanspai2000@gmail.com>
2021-04-01 10:40:36 +0530

    Update CodeCost and TotalEncodingCost

--- metrics.py
+++ metrics.py
@@ -1,5 +1,5 @@
 import numpy as np
-from math import log2, ceil
+from math import log2, ceil, log
 import pandas as pd
 
 
@@ -38,13 +38,48 @@ def DescriptionCost(A):
 	return cost1 + cost2 + cost3
 
 
-def CodeCost(D):
-	return 0
+def CodeCost(D, G, k):
+	n = G.shape[0]
+	cost = 0
+
+	# Iterate for cluster ID
+	for i in range(k):
+		for j in range(k):
+			total = 0
+			ones = 0
+			x = 0
+
+			while x<n:
+				while x<n and G[x] != i+1:
+					x = x + 1
+				if x == n:
+					break
+
+				y = 0
+				while y<n:
+					while y<n and G[y] != j+1:
+						y = y + 1
+					if y == n:
+						break
+
+					total = total + 1
+					if D[x][y] == 1:
+						ones = ones + 1
+
+					y = y + 1
+				x = x + 1
+
+			P = ones / total
+
+			if ones > 0 and ones < total:
+				cost = cost - ones*log(P) - (total - ones)*log(1 - P)
+
+	return cost
 
 
-def TotalEncodingCost(D, A):
+def TotalEncodingCost(D, A, G):
 	description_cost = DescriptionCost(A)
-	code_cost = CodeCost(D)
+	code_cost = CodeCost(D, G, len(A))
 	return description_cost + code_cost
 
 
